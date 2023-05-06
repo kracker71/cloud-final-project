b628074416161d5ed6031f10ddb7fd54649937d9
ChayutAu <modunkung@gmail.com>
2023-05-07 06:08:41 +0700

    Delete some comment

--- frontend/src/tools/mapJson.js
+++ frontend/src/tools/mapJson.js
@@ -1,22 +1,8 @@
 export async function mapJson() {
-    // var res = ""
-    // fetch('https://mmtqpi1os7.execute-api.us-east-1.amazonaws.com/default/getMook',{
-    //     method: 'post',
-    // }).then(response => response.json())
-    // .then(data => {
-    //     console.log(data)
-    //     res = data[text]
-    //     console.log(res)
-    // })
-    // .catch(error => {
-    //     console.log(error)
-    //     return error
-    // });
 
     const res = await fetch('https://mmtqpi1os7.execute-api.us-east-1.amazonaws.com/default/getMook',{
         method: 'post',
     })
-    console.log(res)
     return res
 }
 
